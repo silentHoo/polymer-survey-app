97e1ba0091a132828ab054ee3dfee14157ec8441
Patrick Hillert <patrick.hillert@gmx.de>
2015-02-25 15:58:12 +0100

    Moved theme change function into global app.js.

--- src/app/scripts/app.js
+++ src/app/scripts/app.js
@@ -1,6 +1,16 @@
 (function(document) {
   'use strict';
 
+  document.addEventListener("DOMContentLoaded", function(event) {
+    // it's elegant and clean to change the document things here
+    window.addEventListener('theme-changed', function (e) {
+      var linkEl = document.querySelector('#themeLink');
+      if (linkEl) {
+        linkEl.href = 'styles/theme-' + e.detail.themeName + '.css';
+      }
+    });
+  });
+
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
